3ec4dbf5a96cfac983ca90bce2549892af89ee4e
nilo <nikola.ilo@gmail.com>
2014-02-10 11:39:20 +0100

    fixed js graph/model merge

--- metaservice-frontend/index.js
+++ metaservice-frontend/index.js
@@ -141,6 +141,123 @@ function getProvenanceTitle(element){
     return $(element).text();
 }
 
+
+// source http://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+function deepCompare () {
+    var leftChain, rightChain;
+
+    function compare2Objects (x, y) {
+        var p;
+
+        // remember that NaN === NaN returns false
+        // and isNaN(undefined) returns true
+        if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+            return true;
+        }
+
+        // Compare primitives and functions.
+        // Check if both arguments link to the same object.
+        // Especially useful on step when comparing prototypes
+        if (x === y) {
+            return true;
+        }
+
+        // Works in case when functions are created in constructor.
+        // Comparing dates is a common scenario. Another built-ins?
+        // We can even handle functions passed across iframes
+        if ((typeof x === 'function' && typeof y === 'function') ||
+            (x instanceof Date && y instanceof Date) ||
+            (x instanceof RegExp && y instanceof RegExp) ||
+            (x instanceof String && y instanceof String) ||
+            (x instanceof Number && y instanceof Number)) {
+            return x.toString() === y.toString();
+        }
+
+        // At last checking prototypes as good a we can
+        if (!(x instanceof Object && y instanceof Object)) {
+            return false;
+        }
+
+        if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
+            return false;
+        }
+
+        if (x.constructor !== y.constructor) {
+            return false;
+        }
+
+        if (x.prototype !== y.prototype) {
+            return false;
+        }
+
+        // check for infinitive linking loops
+        if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
+            return false;
+        }
+
+        // Quick checking of one object beeing a subset of another.
+        // todo: cache the structure of arguments[0] for performance
+        for (p in y) {
+            if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                return false;
+            }
+            else if (typeof y[p] !== typeof x[p]) {
+                return false;
+            }
+        }
+
+        for (p in x) {
+            if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
+                return false;
+            }
+            else if (typeof y[p] !== typeof x[p]) {
+                return false;
+            }
+
+            switch (typeof (x[p])) {
+                case 'object':
+                case 'function':
+
+                    leftChain.push(x);
+                    rightChain.push(y);
+
+                    if (!compare2Objects (x[p], y[p])) {
+                        return false;
+                    }
+
+                    leftChain.pop();
+                    rightChain.pop();
+                    break;
+
+                default:
+                    if (x[p] !== y[p]) {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    if (arguments.length < 1) {
+        return true; //Die silently? Don't know how to handle such case, please help...
+        // throw "Need two or more arguments to compare";
+    }
+
+    for (var i = 1, l = arguments.length; i < l; i++) {
+
+        leftChain = []; //todo: this can be cached
+        rightChain = [];
+
+        if (!compare2Objects(arguments[0], arguments[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function jsonLdFollowIris(source,iri){
     if(source[iri]){
         var obj = source[iri];
@@ -171,7 +288,7 @@ function jsonLdFollowIris(source,iri){
                             if(x && x['@id']){
                                 var id = x['@id'];
                                 //break loops
-                                delete d[xindex];
+                            //    delete d[xindex];
                                 d[xindex] = jsonLdFollowIris(source,id);
                             }
                         });
@@ -211,22 +328,39 @@ function jsonLdUnion(array){
                         object[dataIndex] = toProcess;
                     } else {
                         if($.isArray(object[dataIndex]) && $.isArray(toProcess)){
-                            for(var i = 0; i < toProcess.length;i++){
-                                if($.inArray(toProcess[i],object[dataIndex])==-1){
+                            for(i = 0; i < toProcess.length;i++){
+                                found = false;
+                                for(j = 0; j < object[dataIndex].length;j++){
+                                    if(deepCompare(toProcess[i],object[dataIndex][j]))
+                                    found = true;
+                                }
+                                if(!found){
                                     object[dataIndex].push(toProcess[i]);
                                 }
                             }
                         } else if ($.isArray(object[dataIndex])){
-                            if($.inArray(toProcess,object[dataIndex])==-1){
+                            found = false;
+                                for(j = 0; j < object[dataIndex].length;j++){
+                                    if(deepCompare(toProcess,object[dataIndex][j]))
+                                        found = true;
+                                }
+                            if(!found){
                                 object[dataIndex].push(toProcess);
                             }
                         } else if ($.isArray(toProcess)){
-                            if($.inArray(object[dataIndex],toProcess)){
-                                toProcess.push(object[dataIndex]);
-                                object[dataIndex] = toProcess;
+                            found = false;
+                            for(i = 0; i < toProcess.length;i++){
+                                 if(deepCompare(toProcess[i],object[dataIndex]))
+                                    found = true;
                             }
+                            if(!found){
+                                toProcess.push(object[dataIndex]);
+                            }
+                            object[dataIndex] = toProcess;
                         } else {
-                            object[dataIndex] = [object[dataIndex],toProcess];
+                            if(!deepCompare(toProcess,object[dataIndex])){
+                                object[dataIndex] = [object[dataIndex],toProcess];
+                            }
                         }
                     }
                 }
